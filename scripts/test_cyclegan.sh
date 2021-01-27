2ac6ee3c4d7874e7785a19cbb173dc8843315a7a
Marlen Runz <runzmarlen@gmail.com>
2021-01-27 15:42:05 +0100

    cleanup

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -1,2 +1,3 @@
 set -ex
-python test.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --phase test --no_dropout
+python train.py --dataroot ./datasets/ --name tmp --display_id -1
+python test.py --dataroot  --name maps_cyclegan --model cycle_gan --phase test --no_dropout
