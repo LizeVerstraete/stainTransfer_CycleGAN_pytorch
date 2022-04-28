5f05721e050cee34266454fda4ed70bbc400a58a
Marlen Runz <runzmarlen@gmail.com>
2022-04-28 10:43:31 +0200

    test and train scripts for cycleGAN experiments modified

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -1,3 +1,7 @@
 set -ex
-python train.py --dataroot ./datasets/ --name tmp --display_id -1
-python test.py --dataroot  --name maps_cyclegan --model cycle_gan --phase test --no_dropout
+# set '--model_suffix _A' to test the model from A to B, or '_B' vice versa
+
+# normalize from Camelyon16 to HEV stain only
+#python test.py --dataroot /home/mr38/sds_hd/sd18a006/marlen/project_data/pytorch-cycleGan-stain-normalization/camelyon16classification/trainImages/test --name camelyon_onlyH_s256_c128 --model test --phase test --no_dropout --preprocess none --epoch 60 --suffix camelyon --num_test 10000 --model_suffix _A --results_dir /home/mr38/sds_hd/sd18a006/marlen/project_data/pytorch-cycleGan-stain-normalization/camelyon16classification/results/
+# HEV
+python test.py --dataroot /home/mr38/sds_hd/sd18a006/marlen/histoNorm/HEV/trainImages/train_test_sets/size1024_overlap341/test --suffix normalHE --name normalHE_to_onlyE_s1024_c64 --model test --phase test --no_dropout --preprocess none --epoch latest --num_test 100 --model_suffix _A --results_dir /home/mr38/sds_hd/sd18a006/marlen/histoNorm/HEV/trainImages/train_test_sets/size1024_overlap341/results/
