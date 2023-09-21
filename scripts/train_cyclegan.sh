de9bc1e84dea738095b1ab9da8514852a2cf5ae1
marlen <31984397+m4ln@users.noreply.github.com>
2023-09-21 16:14:04 +0200

    Duplicate name removed

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -2,7 +2,7 @@ set -ex
 
 START=$(date +%s.%N)
 
-python train.py --dataroot {path_to_trainA_and_trainB} --name {name_of_experiment} --results_dir {path_to_results} --name {name_of_experiment} --load_size {load_size} --crop_size {crop_size} --pool_size {pool_size} --model cycle_gan
+python train.py --dataroot {path_to_trainA_and_trainB} --name {name_of_experiment} --results_dir {path_to_results} --load_size {load_size} --crop_size {crop_size} --pool_size {pool_size} --model cycle_gan
 
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
